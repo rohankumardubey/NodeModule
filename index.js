7ead489076628c155ed6f63c4c9bd73c977673e2
rohankumardubey <rohankumardubey497@gmail.com>
2019-11-27 03:39:12 +0530

    commits

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const FILE_PATH='./data.json';
 const random = require('random');
 const makeCommit  = n =>{
     if(n===0) return simpleGit.push();
-    const x = random.int(0,54);
+    const x = random.int(0,100);
     const y = random.int(0,7);
     const DATE = moment().subtract(3,'y').add(x,'w').add(y,'d').format();
 
