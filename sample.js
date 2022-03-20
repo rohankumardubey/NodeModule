2e9e35dc534dab3a18144759954e2a968264db4f
rohankumardubey <rohankumardubey497@gmail.com>
2022-03-20 15:19:14 +0530

    data inconsistency

--- sample.js
+++ sample.js
@@ -7,7 +7,7 @@ const makeCommit = n => {
     if(n === 0) simpleGit().push();
     const x = random.int(0,54);
     const y = random.int(0,6);
-    const DATE = moment().subtract(3.1,"y").add(1,"d").add(x,"w").add(y,"d").format();
+    const DATE = moment().subtract(3.2,"y").add(1,"d").add(x,"w").add(y,"d").format();
 
     const data = {
         data:DATE
