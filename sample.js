49bbb00dd2dff51421eed08d1cd7d541c0bb5028
rohankumardubey <rohankumardubey497@gmail.com>
2022-03-20 16:13:22 +0530

    CHANGES

--- sample.js
+++ sample.js
@@ -7,7 +7,7 @@ const makeCommit = n => {
     if(n === 0) simpleGit().push();
     const x = random.int(0,54);
     const y = random.int(0,6);
-    const DATE = moment().subtract(4.2,"y").add(1,"d").add(x,"w").add(y,"d").format();
+    const DATE = moment().subtract(4.21,"y").add(1,"d").add(x,"w").add(y,"d").format();
 
     const data = {
         data:DATE
@@ -15,7 +15,7 @@ const makeCommit = n => {
     console.log(DATE);
     jsonfile.writeFile(FILE_PATH,data);
 
-    simpleGit().add([FILE_PATH]).commit(DATE,{"--date":DATE},makeCommit.bind(this,--n));
+    simpleGit().add([FILE_PATH]).commit(DATE,{"--date":DATE},makeCommit.bind(this,--n));    
 
 };
 
