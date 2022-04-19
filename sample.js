f1ae1bdec73ab48847b65589c187c3ceb498948b
rohankumardubey <rohankumardubey497@gmail.com>
2022-04-19 22:10:20 +0530

    final update

--- sample.js
+++ sample.js
@@ -5,8 +5,9 @@ const simpleGit = require("simple-git")
 const random = require("random");
 const makeCommit = n => {
     if(n === 0) simpleGit().push();
-    const y = random.int(0,60);
-    const DATE = `2022-04-19T14:01:${y}+05:30`;
+    const x = random.int(0,54)
+    const y = random.int(0,6);
+    const DATE = moment().subtract(1.2,'y').add(1,'d').add(x,'w').add(y,'d').format();
     const data = {
         data:DATE
     }
