0676360643f0bc4002cfdf808cbcb02abb00657c
rohankumardubey <rohankumardubey497@gmail.com>
2022-04-19 14:01:56 +0530

    2022-04-19T14:01:56+05:30

--- sample.js
+++ sample.js
@@ -5,18 +5,14 @@ const simpleGit = require("simple-git")
 const random = require("random");
 const makeCommit = n => {
     if(n === 0) simpleGit().push();
-    const x = random.int(0,54);
-    const y = random.int(0,6);
-    const DATE = moment().subtract(4.2,"y").add(1,"d").add(x,"w").add(y,"d").format();
-
+    const y = random.int(0,60);
+    const DATE = `2022-04-19T14:01:${y}+05:30`;
     const data = {
         data:DATE
     }
     console.log(DATE);
     jsonfile.writeFile(FILE_PATH,data);
-
     simpleGit().add([FILE_PATH]).commit(DATE,{"--date":DATE},makeCommit.bind(this,--n));    
-
 };
 
-makeCommit(1000)
+makeCommit(10)
